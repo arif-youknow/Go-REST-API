aab99491dd29de10233d9a73f21fca3ffea00962
arif-youknow <inbox.arifkhan@gmail.com>
2025-08-01 01:39:42 +0600

    basic request-response

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
-
-import(
+import (
 	"fmt"
+	"net/http"
 )
+
+
+func home(w http.ResponseWriter, r *http.Request){
+
+fmt.Fprintln(w, "This is home page")
+
+
+}
+
+func contact(w http.ResponseWriter, r *http.Request){
+
+	fmt.Fprintln(w, "This is contact page")
+}
+
+func about(w http.ResponseWriter, r *http.Request){
+
+fmt.Fprintln(w, "This is about page")
+
+}
 
 
 func main(){
 
+http.HandleFunc("/", home)
+http.HandleFunc("/about", about)
+http.HandleFunc("/contact", contact)
 
-fmt.Println("Go rest api")
 
 
+
+
+
+
+fmt.Println("server running...")
+http.ListenAndServe(":3000", nil)
 }
