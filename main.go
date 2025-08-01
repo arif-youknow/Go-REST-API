d4176be7903ec6867ca95edee997172c9e63d71f
arif-youknow <inbox.arifkhan@gmail.com>
2025-08-01 23:01:56 +0600

    gorilla/mux package installed

--- main.go
+++ main.go
@@ -1,10 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type Task struct{
+	ID int `json: "id"`
+	Title string `json: "title"`
+	Done bool `json: "done"`
+}
+
+
+//Task type er ekta slice globally create korlam
+var tasks []Task
+
+
+
+
+
+func getTasks(w http.ResponseWriter, r *http.Request){
+
+w.Header().Set("Content-Type", "application/json")
+json.NewEncoder(w).Encode(tasks)
+
+}
+
+func createTask(w http.ResponseWriter, r *http.Request){
+
+var task Task
+
+json.NewDecoder(r.Body).Decode(&task)
+task.ID = len(tasks) + 1
+tasks = append(tasks, task)
+
+w.Header().Set("Content-Type", "application/json")
+json.NewEncoder(w).Encode(tasks)
+
+
+
+
+
+
+}
 
 func home(w http.ResponseWriter, r *http.Request){
 
@@ -26,6 +65,22 @@ fmt.Fprintln(w, "This is about page")
 
 
 func main(){
+
+
+http.HandleFunc("/task" , func(w http.ResponseWriter, r *http.Request) {
+
+	if(r.Method=="GET"){
+
+		getTasks(w, r)
+	}else if(r.Method=="POST"){
+		createTask(w,r)
+	}else{
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(w, "Method not allowed")
+	}
+
+
+})
 
 http.HandleFunc("/", home)
 http.HandleFunc("/about", about)
